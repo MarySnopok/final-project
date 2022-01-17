4ce5bed7cd641f79e43814c2e6dfa34769fd2622
MarySnopok <marysnopok8@gmail.com>
2022-01-17 18:05:00 +0100

    Try google maps

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.bs to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} defaultZoom={15} region={{ latitude: 37.8024, longitude: -122.4351 }}>
+        <MapView.Polyline
+          coordinates={[
+            { latitude: 37.8025259, longitude: -122.4351431 },
+            { latitude: 37.7896386, longitude: -122.421646 },
+            { latitude: 37.7665248, longitude: -122.4161628 },
+            { latitude: 37.7734153, longitude: -122.4577787 },
+            { latitude: 37.7948605, longitude: -122.4596065 },
+            { latitude: 37.8025259, longitude: -122.4351431 },
+          ]}
+          path={[
+            { lat: 37.8025259, lng: -122.4351431 },
+            { lat: 37.7896386, lng: -122.421646 },
+            { lat: 37.7665248, lng: -122.4161628 },
+          ]}
+          strokeColor="#ff3333" // fallback for when `strokeColors` is not supported by the map-provider
+          strokeWidth={6}
+        />
+      </MapView>
     </View>
   );
 }
@@ -16,5 +33,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
