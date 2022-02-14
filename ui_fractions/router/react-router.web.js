d5fd8552bf09a58c591951366c03fbbae45e3b58
MarySnopok <marysnopok8@gmail.com>
2022-02-14 14:41:02 +0100

    deleting unessential dependencies

--- ui_fractions/router/react-router.web.js
+++ ui_fractions/router/react-router.web.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Route as WebRoute, Link as WebLink, Routes } from "react-router-dom";
+import { HashRouter, Route as WebRoute, Link as WebLink, Routes } from "react-router-dom";
 
 export const Router = HashRouter;
 export const Route = WebRoute;
