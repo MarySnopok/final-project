e5fd281d67b32ecb39a0204de2f45254cdfb2891
MarySnopok <marysnopok8@gmail.com>
2022-04-05 19:39:19 +0200

    heroku link enabled

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,7 @@
 import colors from "../utils/colors.json";
 
-// const BASE_URL = "https://final-project-backend-hikernav.herokuapp.com";
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://final-project-backend-hikernav.herokuapp.com";
+// const BASE_URL = "http://localhost:8080";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
 
